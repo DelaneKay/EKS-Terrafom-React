7b3939d8dc7197c57547e4e9ec21861f46cb7d9c
DelaneKay <kudakwashedelanesibanda@gmail.com>
2024-02-17 16:12:42 +0200

    variables to s3 names

--- s3-files/main.tf
+++ s3-files/main.tf
@@ -1,17 +1,17 @@
 resource "aws_s3_bucket" "kubernetes-remote-backend" {
-  bucket = "kubernetes-remote-backend"
+  bucket = var.bucket_name
   force_destroy = true 
 }
 
 resource "aws_s3_bucket_versioning" "bucket-versioning" {
-  bucket = "kubernetes-remote-backend"
+  bucket = var.bucket_name
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket-encrypt" {
-  bucket = "kubernetes-remote-backend"
+  bucket = var.bucket_name
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket-encrypt" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name = "terraform-state-locking"
+  name = var.dynamodb_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
   attribute {
---
+++ s3-files/variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_name" {
+  default = "kubernetes-remote-backend"
+}
+
+variable "dynamodb_name" {
+  default = "kubernetes-terraform-locking"
+}
